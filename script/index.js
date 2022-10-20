5d530b2bc518074a3f64e5cb12d6075361e4e717
Lucas Maclean <contatolucasmaclean@gmail.com>
2022-10-19 22:18:10 -0300

    Quarta tarefa realizada com sucesso!

--- script/index.js
+++ script/index.js
@@ -8,12 +8,139 @@ alert(`O cliente abasteceu ${litrosRecarregados.toFixed(2)} litros de gasolina`)
 
 // Segunda Tarefa
 
-let grausCelsius = parseFloat(prompt("Quantos graus Celsius?"))
-let grausFahrenheit = (9 * grausCelsius + 160) / 5
-alert(`A temperatura de ${grausCelsius}ºC em Fahrenheit é de ${grausFahrenheit.toFixed(2)}ºF`)
+let grausCelsius = parseFloat(prompt("Quantos graus Celsius?"));
+let grausFahrenheit = (9 * grausCelsius + 160) / 5;
+alert(`A temperatura de ${grausCelsius}ºC em Fahrenheit é de ${grausFahrenheit.toFixed(2)}ºF`);
 
 // Terceira Tarefa
 
-let gFahrenheit = parseFloat(prompt("Quantos graus Fahrenheit?"))
-let gCelsius = ((gFahrenheit - 32) * 5) / 9
-alert(`A temperatura de ${gFahrenheit}ºF em Celsius é de ${gCelsius.toFixed(2)}ºC`)
+let gFahrenheit = parseFloat(prompt("Quantos graus Fahrenheit?"));
+let gCelsius = ((gFahrenheit - 32) * 5) / 9;
+alert(`A temperatura de ${gFahrenheit}ºF em Celsius é de ${gCelsius.toFixed(2)}ºC`);
+
+// Quarta Tarefa
+
+let comprimento = parseFloat(prompt("Qual o comprimento?"));
+let largura = parseFloat(prompt("Qual a largura?"));
+let altura = parseFloat(prompt("Qual a altura?"));
+let volume = `${comprimento}` * `${largura}` * `${altura}`;
+alert(`O volume é de: ${volume}m³`)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
