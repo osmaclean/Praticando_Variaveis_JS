9a7e39eb447dd1d668cae88eecb6b5861fb2cdf5
Lucas Maclean <contatolucasmaclean@gmail.com>
2022-10-19 22:02:28 -0300

    Segunda tarefa realizada com sucesso!

--- script/index.js
+++ script/index.js
@@ -1,5 +1,14 @@
+// Primeira Tarefa
+
 let valorGasolina = `R$` + 6.8;
 let recargaGasolina = parseFloat(prompt("Quanto em dinheiro deseja abastecer?"));
 let litrosRecarregados = parseFloat(recargaGasolina / 6.8);
-alert(`O cliente abasteceu ${litrosRecarregados.toFixed(2)}`);
-console.log(valorGasolina)
+alert(`O cliente abasteceu ${litrosRecarregados.toFixed(2)} litros de gasolina`);
+
+
+// Segunda Tarefa
+
+let grausCelsius = parseFloat(prompt("Quantos graus Celsius?"))
+let grausFahrenheit = (9 * grausCelsius + 160) / 5
+alert(`A temperatura de ${grausCelsius}ºC em Fahrenheit é de ${grausFahrenheit.toFixed(2)}ºF`)
+
