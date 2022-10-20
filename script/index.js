e183cf64d1e3e9ded3fb369f76ff5e98462d7fac
Lucas Maclean <contatolucasmaclean@gmail.com>
2022-10-19 23:11:31 -0300

    Sétima tarefa realizada com sucesso!

--- script/index.js
+++ script/index.js
@@ -39,12 +39,18 @@ let valor2 = parseFloat(prompt("Escreva o segundo número"));
 let valor3 = parseFloat(prompt("Escreva o terceiro número"));
 let valor4 = parseFloat(prompt("Escreva o quarto número"));
 let valor5 = parseFloat(prompt("Escreva o quinto número"));
-let somaValores = (valor1 + valor2 + valor3 + valor4 + valor5)
-let mediaValores = somaValores / 5
-alert(`A soma dos valores informador é: ${somaValores} e a média das notas é: ${mediaValores}`)
+let somaValores = (valor1 + valor2 + valor3 + valor4 + valor5);
+let mediaValores = somaValores / 5;
+alert(`A soma dos valores informador é: ${somaValores} e a média das notas é: ${mediaValores.toFixed(2)}`);
 
+// Sétima Tarefa
 
+let inteiro1 = parseInt(prompt("Escreva o primeiro número inteiro"));
+let inteiro2 = parseInt(prompt("Escreva o segundo valor inteiro"));
+let restoDivisao = (inteiro1 % inteiro2 )
+alert(`O resto da divisão de ${inteiro1} por ${inteiro2} é: ${restoDivisao}`)
 
+// Obs: Para calcular o resto usa-se o sinal de percentagem %
 
 
 
